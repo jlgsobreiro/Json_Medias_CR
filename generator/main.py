80e4283bb7689d51e52396f17d27fca6bae0b925
jlgsobreiro <jlgsobreiro@gmail.com>
2018-05-25 22:05:58 -0300

    Tkinter adcionado + interface grafica simples

--- generator/main.py
+++ generator/main.py
@@ -1,1 +1,20 @@
-abc =""
+from tkinter import *
+
+def Cumprimente():
+    hello.set("Olá, você!")
+
+gui = Tk()
+
+gui.title("Py5 - Python + Tkinter")
+gui.geometry("400x300")
+
+btn = Button(gui, text="Cumprimente", command=Cumprimente)
+texto = Text(gui, width=30, height=1)
+texto.pack()
+btn.pack()
+
+hello = StringVar()
+lbl = Label(gui, textvariable=hello)
+lbl.pack()
+
+gui.mainloop()
